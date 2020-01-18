4f9941c06b7844b11674df2ac1703ba501643293
nk-5 <nkws27@gmail.com>
2020-01-18 12:32:41 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Keigo Nakagawa on 2020/01/18.
+//  Copyright © 2020 Keigo Nakagawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
